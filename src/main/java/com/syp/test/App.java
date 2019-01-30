43abcf97a70ca4e7da85d4f35204f0b7ceb818f9
syp2850 <512121086@qq.com>
2019-01-30 12:07:09 +0800

    first commit

---
+++ src/main/java/com/syp/test/App.java
@@ -0,0 +1,13 @@
+package com.syp.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
